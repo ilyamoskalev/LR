644cda3e3ac092cb3bf9d79c37a834829082b6d2
ilyamoskalev <ilyamoskalev@mail.ru>
2016-10-13 17:08:20 +0300

    Add files via upload

---
+++ tests_catch/build/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test.dir/source/func.cpp.o"
+  "CMakeFiles/Test.dir/tests/main.cpp.o"
+  "libTest.pdb"
+  "libTest.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
